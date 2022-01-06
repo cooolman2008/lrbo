e0977b53b5637dcaec4117ee2a7be27b4c0eafcf
Subrahmanya M <cooolman2008@gmail.com>
2022-01-06 15:54:14 +0530

    feat: addition

--- front-page.php
+++ front-page.php
@@ -15,15 +15,38 @@
 get_header();
 ?>
 <section id="lrbo-home-page-root">
+    <div class="lrbo-title">
+        <h1 class="">Love Revolution<br>2022<br>[In Love We Trust]</h1>
+        <p>Welcome to the Love Revolution. This movement was inspired by the German Author Micheal Ende
+            with his book MOMO.This Movement is made by Children. Vision of this ovement is to come back
+            to the basics.</p>
+    </div>
 </section>
-<section class="lrbo-section" >
+<section class="lrbo-section" id="about">
+    <div class="lrbo-title">
+        <h1 class="">About</h1>
+        <p>Life starts with mothers Love & should end with blessings from love only.</p>
+        <p>It is a time o live & peace on this planet. To bring back the smiles on their beautiful faces</p>
+        <p>We want to dedicate this Revolution to all children in this World and to give this planet back to them.</p>
+        <p>It is our duty now to bring back the sun.</p>
+        <p>To give them back spcae to play & run.</p>
+        <p>To support them in a healthy growing, way that contains Body Mind & Soul</p>    </div>
 </section>
-<section class="lrbo-section">
+<section class="lrbo-section" id="vision">
+    <div class="lrbo-title">
+        <h1 class="">Love Revolution<br>2022<br>[In Love We Trust]</h1>
+        <p>Welcome to the Love Revolution. This movement was inspired by the German Author Micheal Ende
+            with his book MOMO.This Movement is made by Children. Vision of this ovement is to come back
+            to the basics.</p>
+    </div>
 </section>
-<section class="lrbo-section">
-    <?php 
-        include(locate_template('reference.php'));
-    ?>
+<section class="lrbo-section" id="join">
+    <div class="lrbo-title">
+        <h1 class="">Love Revolution<br>2022<br>[In Love We Trust]</h1>
+        <p>Welcome to the Love Revolution. This movement was inspired by the German Author Micheal Ende
+            with his book MOMO.This Movement is made by Children. Vision of this ovement is to come back
+            to the basics.</p>
+    </div>
 </section>
 
 <?php
