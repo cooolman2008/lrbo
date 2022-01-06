6c60d6db072a77846c33c43bce692a6befed7bbf
Subrahmanya M <cooolman2008@gmail.com>
2022-01-07 02:29:44 +0530

    feat: add

--- front-page.php
+++ front-page.php
@@ -17,9 +17,9 @@ get_header();
 <section id="lrbo-home-page-root">
     <div class="lrbo-title">
         <h1 class="">Love Revolution<br>2022<br>[In Love We Trust]</h1>
-        <p>Welcome to the Love Revolution. This movement was inspired by the German Author Micheal Ende
-            with his book MOMO.This Movement is made by Children. Vision of this ovement is to come back
-            to the basics.</p>
+        <p>Welcome to the Love Revolution.</p>
+        <p>This movement was inspired by the German Author Micheal Ende with his book MOMO</p>
+        <p>This Movement is made by Children. Vision of this ovement is to come back to the basics.</p>
     </div>
 </section>
 <section class="lrbo-section" id="about">
@@ -30,14 +30,24 @@ get_header();
         <p>We want to dedicate this Revolution to all children in this World and to give this planet back to them.</p>
         <p>It is our duty now to bring back the sun.</p>
         <p>To give them back spcae to play & run.</p>
-        <p>To support them in a healthy growing, way that contains Body Mind & Soul</p>    </div>
+        <p>To support them in a healthy growing, way that contains Body Mind & Soul.</p>
+        <p>To save them from any kind of danger in the future.</p>
+        <p>To potect their space, their rights, their happiness & their future</p>
+        <p>Our duty sould be to protect the devine Mothers & Fathers, because they are taing care of our real future.</p>
+    </div>
 </section>
 <section class="lrbo-section" id="vision">
     <div class="lrbo-title">
-        <h1 class="">Love Revolution<br>2022<br>[In Love We Trust]</h1>
-        <p>Welcome to the Love Revolution. This movement was inspired by the German Author Micheal Ende
-            with his book MOMO.This Movement is made by Children. Vision of this ovement is to come back
-            to the basics.</p>
+        <h1 class="">Your support</h1>
+        <h2 class="">Welcome Love Revolution with open arms</h2>
+        <h2 class="">Om Shanti Om Shanti Shanti Om</h2>
+        <p>Take Action, Take responsibility</p>
+        <p>Let's start to build the new world for them without fear anymore.</p>
+        <p>Take Action, Take responsibility</p>
+        <p>Take Action, Take responsibility</p>
+        <p>Take Action, Take responsibility</p>
+        <p>Take Action, Take responsibility</p>
+        <p>Take Action, Take responsibility</p>
     </div>
 </section>
 <section class="lrbo-section" id="join">
