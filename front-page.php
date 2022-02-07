22022c1b4473fd77b4ba88659e6e4f047d98fd3b
Subrahmanya M <cooolman2008@gmail.com>
2022-02-07 23:05:35 +0530

    feat: added some improvements

--- front-page.php
+++ front-page.php
@@ -16,7 +16,7 @@ get_header();
 ?>
 <section id="lrbo-home-page-root">
     <div class="lrbo-title">
-        <h1 class="">Love Revolution<br>2022<br>[In Love We Trust]</h1>
+        <h1 class="">Love Revolution<br>2022<br>{ In Love We Trust }</h1>
         <p>Welcome to the Love Revolution.</p>
         <p>This movement was inspired by the German Author Micheal Ende with his book MOMO</p>
         <p>This Movement is made by Children. Vision of this ovement is to come back to the basics.</p>
@@ -24,9 +24,9 @@ get_header();
 </section>
 <section class="lrbo-section" id="about">
     <div class="lrbo-title">
-        <h1 class="">About</h1>
+        <h1 class="">About Love Revolution</h1>
         <p>Life starts with mothers Love & should end with blessings from love only.</p>
-        <p>It is a time o live & peace on this planet. To bring back the smiles on their beautiful faces</p>
+        <p>It is a time to live in peace on this planet. To bring back the smiles on their beautiful faces</p>
         <p>We want to dedicate this Revolution to all children in this World and to give this planet back to them.</p>
         <p>It is our duty now to bring back the sun.</p>
         <p>To give them back spcae to play & run.</p>
@@ -40,22 +40,31 @@ get_header();
     <div class="lrbo-title">
         <h1 class="">Your support</h1>
         <h2 class="">Welcome Love Revolution with open arms</h2>
-        <h2 class="">Om Shanti Om Shanti Shanti Om</h2>
+        <h2 class="">!! Om Shanti Om Shanti Shanti Om !!</h2>
         <p>Take Action, Take responsibility</p>
         <p>Let's start to build the new world for them without fear anymore.</p>
-        <p>Take Action, Take responsibility</p>
-        <p>Take Action, Take responsibility</p>
-        <p>Take Action, Take responsibility</p>
-        <p>Take Action, Take responsibility</p>
-        <p>Take Action, Take responsibility</p>
+        <p>This Revolution should bring so much love back to Mother Earth.</p>
+        <p>Our biggest wish should be to bring this revolution to all children around the world so they can start 2022 with a "New" world with lots of love & family time.</p>
+        <p>Our children need our support now.</p>
+        <p>Lets give the, their faith back, their life back, their families back. Give back the air to breath.</p>
+        <p>So let's give the world back to them, we should trust them.</p>
+        <p>Then the crown belongs on the children's head.</p>
+        <h2>Always be happy nd let others be happy.</h2>
     </div>
 </section>
 <section class="lrbo-section" id="join">
     <div class="lrbo-title">
-        <h1 class="">Love Revolution<br>2022<br>[In Love We Trust]</h1>
-        <p>Welcome to the Love Revolution. This movement was inspired by the German Author Micheal Ende
-            with his book MOMO.This Movement is made by Children. Vision of this ovement is to come back
-            to the basics.</p>
+        <h1 class="">How to Join Us</h1>
+        <p>To be part of this eautiful movement & support the change, join the Love Revolution Familia.</p>
+        <p>Please be a beautiful artist & show the world your own painted T-shirt with our message "Love Revolution" & your country name.</p>
+        <p>Take a picture & send it to us on facebook/instagram.</p>
+        <p>So that you show is that you are a beautiful member of la Love revolution Familia.</p>
+        <p>We grow together to be a big big family.</p>
+        <h2 class="">We love you</h2>
+        <h2 class="">We respect you</h2>
+        <h2 class="">We support you</h2>
+        <h2 class="">We welcome you</h2>
+        <h2 class="">!! Stay safe. All god's bless you !!</h2>
     </div>
 </section>
 
